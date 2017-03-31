a036961c6d3ca8794ba2477f83a4871081eb9da3
Unknown <hungnt.dao@gmail.com>
2017-03-31 13:29:18 +0300

    PHP pages created (from example2)

---
+++ PHP pages/menu.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PHP example2</title>
+    <link rel="stylesheet" href="mystyle.css">
+    <script type="text/javascript" src="myFunctions.js">
+    </script>
+  </head>
+  <body>
+    <div id="menu">
+      <ul>
+        <li><a href="index.php">Main Page</a></li>
+        <li><a href="login.php">Log In</a></li>
+        <li><a href="logout.php">Log Out</a></li>
+        <li><a href="user.php">User Page</a></li>
+      </ul>
+    </div>
+    <div id="content">
