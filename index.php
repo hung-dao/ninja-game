4da027a9ac7a2ed68ceb77f449ebeaac09e48315
Fayjus Salehin <fsalehin@live.com>
2017-04-04 16:44:47 +0300

    index edited

--- index.php
+++ index.php
@@ -1,25 +1,22 @@
 <html>
   <head>
-    <link rel="stylesheet" href="css/styles.css">
+
+    <!-- Bootstrap, Jquery and p5 CDN -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.8/p5.js"></script>
     <script src="p5/libraries/p5.play.js"></script>
     <script src="p5/libraries/p5.dom.js"></script>
 
+    <link rel="stylesheet" href="css/styles.css">
+
     <script src="js/ninja-game-main.js"></script>
     <script src="js/Ninja.js"></script>
     <script src="js/Enemy.js"></script>
-    <script src="js/navbar.js"></script>
   </head>
 
   <body>
-    <div class="topnav" id="myTopnav">
-        <a href="#home">Home</a>
-        <a href="#news">News</a>
-        <a href="#contact">Contact</a>
-        <a href="#about">About</a>
-        <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
-    </div>
-
     <div class="centered"></div>
   </body>
 </html>
