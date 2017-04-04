fd8278904ca95f11fdbe4a9e9a51b67710827e3e
Unknown <dair.baidauletov@gmail.com>
2017-04-04 11:47:10 +0300

    More modification to Design doc

---
+++ js/Collider.js
@@ -0,0 +1,34 @@
+function Collider()
+{
+	this.x = random (800, 1600) ;
+	this.y = random(0, 565) ;
+   this.width ;
+   this.height ;
+	this.speed ;
+	
+	this.show = function()
+	{
+		fill(144) ;
+		rect(this.x, this.y, this.width, this.height) ;
+	}
+	
+	this.move = function()
+	{
+		this.x -= this.speed ;
+   }
+    
+   this.crash = function(obj)
+   {
+		if (this.x + this.width >= obj.x &&    // r1 right edge past r2 left
+      	 this.x <= obj.x + obj.width &&    // r1 left edge past r2 right
+      	 this.y + this.height >= obj.y &&    // r1 top edge past r2 bottom
+      	 this.y <= obj.y + obj.height) // r1 bottom edge past r2 top
+		{
+			return true ;
+		}
+		else
+		{
+			return false ;
+		}     
+	}
+}
