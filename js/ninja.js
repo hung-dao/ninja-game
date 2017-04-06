a3312546df3f6a05b1a09bc2a69b5134f61c4aec
Unknown <dair.baidauletov@gmail.com>
2017-04-07 00:23:44 +0300

    At least something at last

--- js/ninja.js
+++ js/ninja.js
@@ -1,6 +1,6 @@
 function Ninja()
 {
-	this.height = 60 ;
+	this.height = 33 ;
 	this.width = 40 ;
 	this.x = 20 ;
 	this.y = 100;
@@ -8,11 +8,13 @@ function Ninja()
 	this.yspeed = 0 ;
 	this.health = 100 ;
    this.score = 0;
+	this.run_sprite = player_run ;
 	
 	this.show = function()
 	{
 		fill(255) ;
-		rect(this.x, this.y, this.width, this.height) ;
+		animation(this.run_sprite, (this.x + this.width/2), this.y + (this.height/2)) ;
+		//rect(this.x, this.y, this.width, this.height) ;
 	}
 
 	this.move = function()
