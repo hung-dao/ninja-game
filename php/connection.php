e694c74c76c7fac62a60d020f82d477b51a8148a
Unknown <hungnt.dao@gmail.com>
2017-04-07 11:41:19 +0300

    Show high scores

--- php/connection.php
+++ php/connection.php
@@ -2,3 +2,18 @@
 	$db = mysql_connect('localhost', 'root', 'sql') or die('Failed to connect: ' . mysql_error());
     mysql_select_db('highscores') or die('Failed to access database');
 ?>
+
+<?php
+	try
+	{
+	 $dsn = "mysql:host=localhost;dbname=highscores";
+	 $db = new PDO ($dsn, "root", "sql");
+	 #print ("Connected\n");
+	}
+	catch (PDOException $e)
+	{
+	 print ("Cannot connect to server\n");
+	 print ("Error code: " . $e->getCode () . "\n");
+	 print ("Error message: " . $e->getMessage () . "\n");
+	}
+?>
