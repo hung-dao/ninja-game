bce75521a81f65222d8d297823a08495611e5c68
Fayjus Salehin <fsalehin@live.com>
2017-04-28 02:28:55 +0300

    UI final version

--- index.php
+++ index.php
@@ -36,11 +36,10 @@
 
 <body>
     <section class="panel home" data-section-name="home">
-
-        <img id="logo" src="assets/oamk_logo.png" alt="oamk logo">
-        <div class="inner">
+      <a target="_blank" href="http://www.oamk.fi/fi/"><img id="logo" src="assets/oamk_logo.png" alt="oamk logo"></a>
+      <div class="inner">
             <div class="vertical-center">
-                <h2 class="animated fadeInDownBig">Ninja Game</h2>
+                <h1 class="animated fadeInDownBig">Ninja Game</h1>
                 <p>A simple web based 2D endless running game</p>
                 <p>made with <span class="fa fa-heart"></span> by a bunch of OAMK students as a first yeart project</p>
                 <a href="#play" class="scroll"></a>
@@ -49,28 +48,58 @@
 
         <ul class="pagination">
             <li><a href="#home" class="active"><span class="hover-text">Home</span></a></li>
+            <li><a href="#instruction"><span class="hover-text">Instructions</span></a></li>
             <li><a href="#play"><span class="hover-text">Play</span></a></li>
             <li><a href="#stats"><span class="hover-text">Stats</span></a></li>
-            <li><a href="#about"><span class="hover-text">About</span></a></li>
+            <li><a href="#credits"><span class="hover-text">Credits</span></a></li>
+            <li><a href="#about"><span class="hover-text">About Us</span></a></li>
         </ul>
+    </section>
+
+    <section class="panel instruction" data-section-name="instruction">
+        <div class="inner">
+            <div class="vertical-center">
+                <h2>Instructions</h2>
+                <table>
+                    <tr>
+                        <th>Movement</th>
+                        <th>Key</th>
+                    </tr>
+                    <tr>
+                        <td>Up</td>
+                        <td><b>W</b></td>
+                    </tr>
+                    <tr>
+                        <td>Down</td>
+                        <td><b>S</b></td>
+                    </tr>
+                    <tr>
+                        <td>Left</td>
+                        <td><b>A</b></td>
+                    </tr>
+                    <tr>
+                        <td>Right</td>
+                        <td><b>D</b></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
     </section>
 
     <section class="panel play" data-section-name="play">
         <div id="dialogoverlay"></div>
         <div id="dialogbox">
-          <div>
-            <div id="dialogboxhead"></div>
-            <div id="dialogboxbody"></div>
-            <div id="dialogboxfoot"></div>
-          </div>
+            <div>
+                <div id="dialogboxhead"></div>
+                <div id="dialogboxbody"></div>
+                <div id="dialogboxfoot"></div>
+            </div>
         </div>
-        
+
         <div class="inner">
             <div class="vertical-center" id="canvas-holder">
-            </div>    
+            </div>
         </div>
-        
-        
     </section>
 
     <section class="panel stats" data-section-name="stats">
@@ -101,6 +130,24 @@
         </div>
     </section>
 
+    <section class="panel credits" data-section-name="credits">
+        <div class="inner">
+            <div class="vertical-center">
+                <h2>Credits</h2>
+                <h3>Special thanks to all people who unknowingly contributed to our project </h3>
+          <ul class="credits-list">
+            <li class="credits-list-li"><a class="credits-list-a" target="_blank" href="https://p5js.org">p5 Community </a>and its founder <a class="credits-list-a" target="_blank" href="http://lauren-  mccarthy.com/">Lauren McCarthy</a></li>
+            <li class="credits-list-li"><a class="credits-list-a" target="_blank" href="https://opengameart.org/users/dezrasdragons">DezrasDragon</a> from opengameart.org community for lovely Ninja animations</li>
+            <li class="credits-list-li"><a class="credits-list-a" target="_blank" href="https://opengameart.org/users/ansimuz">ansimuz</a> for breath-taking backgrounds</li>
+            <li class="credits-list-li"><a class="credits-list-a" target="_blank" href="https://opengameart.org/users/sacio">sacio</a> and <a class="credits-list-a" target="_blank" href="https://opengameart.org/users/kungfu4000">kungfu4000</a> for awesome game assets and animations</li>
+            <li class="credits-list-li">Special thanks to <a class="credits-list-a" target="_blank" href="https://opengameart.org">opengameart</a>community</li>
+            <li class="credits-list-li">Custom alert code is inspired by <a class="credits-list-a" target="_blank" href="http://www.developphp.com/video/JavaScript/Custom-Prompt-Box-Programming-Tutorial">developphp.com tutorials</a></li>
+            <li class="credits-list-li">If you want to download the soundtrack, feel free to email at <a class="credits-list-a" href="mailto:dair.baidauletov@gmail.com">dair.baidauletov@gmail.com</a></li>
+          </ul>
+            </div>
+        </div>
+    </section>
+
     <section class="panel about" data-section-name="about">
         <div class="inner profiles">
             <div class="vertical-center">
@@ -109,8 +156,6 @@
                     <div class="profile-image"><img src="assets/profile/dair.jpg" alt="sample47" /></div>
                     <figcaption>
                         <h3>Dair Baidauletov</h3>
-                        <h4>Founder</h4>
-                        <p>Which is worse, that everyone has his price, or that the price is always so low.</p>
                         <div class="icons"><a href="https://www.facebook.com/dair.baidauletov"><i class="fa fa-facebook-official"></i></a>
                             <a href="https://github.com/dairbuirabass"> <i class="fa fa-github"></i></a>
                             <a href="mailto:dair.baidauletov@gmail.com.com"> <i class="fa fa-envelope"></i></a>
@@ -121,8 +166,6 @@
                     <div class="profile-image"><img src="assets/profile/fayjus.jpg" alt="sample47" /></div>
                     <figcaption>
                         <h3>Fayjus Salehin</h3>
-                        <h4>Web Designer</h4>
-                        <p>I'm killing time while I wait for life to shower me with meaning and happiness.</p>
                         <div class="icons"><a href="https://www.facebook.com/fayjusss"><i class="fa fa-facebook-official"></i></a>
                             <a href="https://github.com/fayjusss"> <i class="fa fa-github"></i></a>
                             <a href="mailto:fsalehin@live.com"> <i class="fa fa-envelope"></i></a>
@@ -133,8 +176,6 @@
                     <div class="profile-image"><img src="assets/profile/hung.jpg" alt="sample47" /></div>
                     <figcaption>
                         <h3>Hung Dao</h3>
-                        <h4>Public Relations</h4>
-                        <p>The only skills I have the patience to learn are those that have no real application in life. </p>
                         <div class="icons"><a href="https://www.facebook.com/hung.dao.nt"><i class="fa fa-facebook-official"></i></a>
                             <a href="https://github.com/hungntdao"> <i class="fa fa-github"></i></a>
                             <a href="#about"> <i class="fa fa-envelope"></i></a>
